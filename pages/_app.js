6b2bae419c9f690c064c904b24fbcb6809840d89
kakinglow <kakingklow@gmail.com>
2021-10-28 19:09:34 +0100

    added darkmodeswitch, fonts and theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import "@fontsource/open-sans"
+import "@fontsource/raleway"
+
+import { ChakraProvider, ColorModeProvider, useColorMode } from '@chakra-ui/react'
+import customTheme from '../styles/theme'
 
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider resetCSS theme={customTheme}>
+      <ColorModeProvider
+        options={{
+          initialColorMode: "light",
+          useSystemColorMode: true,
+        }}
+      >
+        <Component {...pageProps} />
+      </ColorModeProvider>
     </ChakraProvider>
   )
 }
