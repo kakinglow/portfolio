bae62eb6cd7af8e11756542ee1496b8ec3599500
kakinglow <kakingklow@gmail.com>
2021-10-28 20:54:56 +0100

    added project.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,35 @@ import Container from '../components/Container'
 
 
 export default function Home() {
+  const { colorMode } = useColorMode()
+  const colorSecondary = {
+    light: 'gray.700',
+    dark: 'gray.400'
+  }
   return (
     <Container>
-      <Text>Hello!</Text>
+      <Head>
+        <title>Temp title</title>
+      </Head>
+      <Stack
+        as="main"
+        spacing={8}
+        justifyContent="center"
+        alignItems="flex-start"
+        m="0 auto 4rem auto"
+        maxWidth="700px"
+        px={2}
+      >
+        <Flex
+          flexDirection="column"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          maxWidth="700px"
+        >
+          <Heading mb={2}>Hi, I am Ka King Low</Heading>
+          <Text color={colorSecondary[colorMode]}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus a ipsum eget elit malesuada eleifend at quis lectus. Quisque porta volutpat enim, at bibendum orci rutrum vel. Etiam bibendum elementum erat, eget rutrum mi ullamcorper sed. Nulla scelerisque ultrices scelerisque. Curabitur a leo eget purus tincidunt dictum</Text>
+        </Flex>
+      </Stack>
     </Container>
   )
 }
